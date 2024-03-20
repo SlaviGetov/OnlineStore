2ef423679a2d1c9c0fca5ddc3b98d59b42b72a08
Slavi Getov <gamera9112@gmail.com>
2024-03-20 12:08:27 +0200

    Changed deprecated, added TODO

--- src/main/java/com/failedsaptrainees/onlinestore/security/SecurityConfig.java
+++ src/main/java/com/failedsaptrainees/onlinestore/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig  {
 
 
+    //TODO: Remove the permit all when authentication (and a login page) is added
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
     }
